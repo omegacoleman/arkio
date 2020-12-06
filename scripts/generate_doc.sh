d3d41d039c4b8487539969c81d6250196989b361
youcai <omegacoleman@gmail.com>
2020-12-06 18:31:22 +0800

    fix a typo in script

--- scripts/generate_doc.sh
+++ scripts/generate_doc.sh
@@ -3,7 +3,7 @@
 export PYTHONDONTWRITEBYTECODE=1
 export CLANG_RESOURCE_DIR=$(clang -print-resource-dir)
 ./vendor/m.css/documentation/doxygen.py ./conf.py
-rm -rf ./doc
-mv ./doxy/html ./doc
+rm -rf ./docs
+mv ./doxy/html ./docs
 rm -rf ./doxy
 
